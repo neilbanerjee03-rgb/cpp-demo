0b798091675c4fd6ebef08ebb2347e6946f0a551
neilbanerjee03-rgb <divabanerjee03@gmail.com>
2025-09-14 10:02:29 +0530

    Add files via upload

---
+++ DAY 1.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	float c,f;
+	cout<<"enter the temperature in celsius :-";
+	cin>>c;
+	f=(1.8*c)+32;
+	cout<<"your temperature in fahrenheit is :- "<<f;
+	return 0;
+	
+}
+
+
+/* we learnt in day one 1) basic structure of c++ 2) output 3) variables and data types 4) arithmetic operators 5) using cin */
+
